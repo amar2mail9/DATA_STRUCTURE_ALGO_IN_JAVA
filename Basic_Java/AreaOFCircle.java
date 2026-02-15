b1cee48d737998e448398947563d4976b0a58f82
Amar Kumae <amar2mail9.com>
2026-02-15 09:00:59 +0530

    area of

--- Basic_Java/AreaOFCircle.java
+++ Basic_Java/AreaOFCircle.java
@@ -4,6 +4,7 @@ public class AreaOFCircle {
     public static void main(String[] args) {
         int radius = 10;
         System.out.println(3.14 * radius * radius);
+        System.out.println(3.14 * radius * radius * radius);
 
     }
 }
