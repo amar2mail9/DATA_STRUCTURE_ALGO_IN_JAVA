d6043ab821414284137db1668cbb123c8b3e2a89
Amar Kumae <amar2mail9.com>
2026-02-15 08:50:37 +0530

    Initial project version

---
+++ Basic_Java/PrintingNumber.java
@@ -0,0 +1,7 @@
+package Basic_Java;
+
+public class PrintingNumber {
+    public static void main(String[] args) {
+        System.out.println(420);
+    }
+}
---
+++ Basic_Java/VariablesBasics.java
@@ -0,0 +1,13 @@
+package Basic_Java;
+
+public class VariablesBasics {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Value of X: " + x);
+        int y = 10;
+        System.out.println("Value of Y : " + y);
+
+        double zcd;
+
+    }
+}
